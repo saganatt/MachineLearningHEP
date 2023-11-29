8aec6b5eebb1d255abe7fbc3c3befb7eeff9592d
Maja Kabus <mkabus@cern.ch>
2023-11-29 10:17:00 +0100

    Adjusted part of correlations.py to multiclass

--- machine_learning_hep/correlations.py
+++ machine_learning_hep/correlations.py
@@ -18,7 +18,6 @@ Methods for correlation and variable plots
 import pickle
 from os.path import join
 from collections import deque
-from io import BytesIO
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -29,29 +28,28 @@ from machine_learning_hep.logger import get_logger
 
 #mpl.use('Agg')
 
-def vardistplot(dataframe_sig_, dataframe_bkg_, mylistvariables_, output_,
+def vardistplot(dfs_input_, mylistvariables_, output_,
                 binmin, binmax, plot_options_):
     mpl.rcParams.update({"text.usetex": True})
     plot_type_name = "prob_cut_scan"
     plot_options = plot_options_.get(plot_type_name, {}) \
             if isinstance(plot_options_, dict) else {}
 
-
+    colors = ['r', 'b', 'g']
     figure = plt.figure(figsize=(20, 15)) # pylint: disable=unused-variable
 
     figure.suptitle(f"Separation plots for ${binmin} < p_\\mathrm{{T}}/(\\mathrm{{GeV}}/c) < " \
                     f"{binmax}$", fontsize=30)
-    i = 1
-    for var in mylistvariables_:
-        ax = plt.subplot(3, int(len(mylistvariables_)/3+1), i)
+    for ind, var in enumerate(mylistvariables_):
+        ax = plt.subplot(3, int(len(mylistvariables_)/3+1), ind)
         plt.yscale('log')
         kwargs = dict(alpha=0.3, density=True, bins=100)
         po = plot_options.get(var, {})
         if "xlim" in po:
             kwargs["range"] = (po["xlim"][0], po["xlim"][1])
 
-        plt.hist(dataframe_sig_[var], facecolor='b', label='signal', **kwargs)
-        plt.hist(dataframe_bkg_[var], facecolor='g', label='background', **kwargs)
+        for label, color in zip(dfs_train, colors):
+            plt.hist(dfs_input_[label][var], facecolor=color, label=label, **kwargs)
 
         var_tex = var.replace("_", ":")
         if "xlim" in po:
@@ -61,16 +59,11 @@ def vardistplot(dataframe_sig_, dataframe_bkg_, mylistvariables_, output_,
         plt.xlabel(var_tex, fontsize=11)
         plt.ylabel(po.get("ylabel", "entries"), fontsize=11)
         ax.legend()
-        i = i+1
     plotname = output_+'/variablesDistribution_nVar%d_%d%d.png' % \
                             (len(mylistvariables_), binmin, binmax)
     plt.savefig(plotname, bbox_inches='tight')
-    imagebytesIO = BytesIO()
-    plt.savefig(imagebytesIO, format='png')
-    imagebytesIO.seek(0)
     mpl.rcParams.update({"text.usetex": False})
     plt.close(figure)
-    return imagebytesIO
 
 def vardistplot_probscan(dataframe_, mylistvariables_, modelname_, thresharray_, # pylint: disable=too-many-statements
                          output_, suffix_, opt=1, plot_options_=None):
@@ -94,6 +87,8 @@ def vardistplot_probscan(dataframe_, mylistvariables_, modelname_, thresharray_,
     xrange_min = []
     xrange_max = []
     ref_hists = []
+    get_logger().info("dataframe:")
+    get_logger().info(dataframe_)
     for thresh_index, threshold in enumerate(thresharray_):
         selml = f"y_test_prob{modelname_}>{threshold}"
         if df_skimmed is None:
@@ -284,34 +279,27 @@ def picklesize_cutscan(dataframe_, mylistvariables_, output_, suffix_, plot_opti
     plotname = join(output_, f"variables_cutscan_picklesize_{suffix_}.png")
     plt.savefig(plotname, bbox_inches='tight')
 
-def scatterplot(dataframe_sig_, dataframe_bkg_, mylistvariablesx_,
+
+def scatterplot(dfs_input_, mylistvariablesx_,
                 mylistvariablesy_, output_, binmin, binmax):
+    colors = ['r', 'b', 'g']
     figurecorr = plt.figure(figsize=(30, 20)) # pylint: disable=unused-variable
-    i = 1
-    for j, _ in enumerate(mylistvariablesx_):
-        axcorr = plt.subplot(3, int(len(mylistvariablesx_)/3+1), i)
-        plt.xlabel(mylistvariablesx_[j], fontsize=11)
-        plt.ylabel(mylistvariablesy_[j], fontsize=11)
-        plt.scatter(
-            dataframe_bkg_[mylistvariablesx_[j]], dataframe_bkg_[mylistvariablesy_[j]],
-            alpha=0.4, c="g", label="background")
-        plt.scatter(
-            dataframe_sig_[mylistvariablesx_[j]], dataframe_sig_[mylistvariablesy_[j]],
-            alpha=0.4, c="b", label="signal")
-        plt.title(
-            'Pearson sgn: %s' %
-            dataframe_sig_.corr().loc[mylistvariablesx_[j]][mylistvariablesy_[j]].round(2)+
-            ',  Pearson bkg: %s' %
-            dataframe_bkg_.corr().loc[mylistvariablesx_[j]][mylistvariablesy_[j]].round(2))
+    for ind in range(len(mylistvariablesx_)):
+        axcorr = plt.subplot(3, int(len(mylistvariablesx_)/3+1), ind)
+        plt.xlabel(mylistvariablesx_[ind], fontsize=11)
+        plt.ylabel(mylistvariablesy_[ind], fontsize=11)
+        title_str = 'Pearson coef. '
+        for label, color in zip(dfs_train, colors):
+            plt.scatter(
+                dfs_input_[label][mylistvariablesx_[ind]], dfs_input_[label][mylistvariablesy_[ind]],
+                alpha=0.4, c=color, label=label)
+            pearson = dfs_input_[label].corr().loc[mylistvariablesx_[ind]][mylistvariablesy_[ind]].round(2)
+            title_str += f'{label}: {pearson}, '
+        plt.title(title_str)
         axcorr.legend()
-        i = i+1
     plotname = output_+'/variablesScatterPlot%d%d.png' % (binmin, binmax)
     plt.savefig(plotname, bbox_inches='tight')
-    imagebytesIO = BytesIO()
-    plt.savefig(imagebytesIO, format='png')
-    imagebytesIO.seek(0)
     plt.close(figurecorr)
-    return imagebytesIO
 
 
 def correlationmatrix(dataframe, mylistvariables, label, output, binmin, binmax,
@@ -341,9 +329,5 @@ def correlationmatrix(dataframe, mylistvariables, label, output, binmin, binmax,
     ax.text(0.7, 0.9, f"${binmin} < p_\\mathrm{{T}}/(\\mathrm{{GeV}}/c) < {binmax}$\n{label}",
             verticalalignment='center', transform=ax.transAxes, fontsize=13)
     plt.savefig(output, bbox_inches='tight')
-    imagebytesIO = BytesIO()
-    plt.savefig(imagebytesIO, format='png')
-    imagebytesIO.seek(0)
     mpl.rcParams.update({"text.usetex": False})
     plt.close()
-    return imagebytesIO
