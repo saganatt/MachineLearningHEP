386d5b7254e688b4e3a24907ef34e543b6c089dd
saganatt <8majak8@gmail.com>
2024-05-23 18:47:21 +0200

    Update gather script to new machine

--- machine_learning_hep/gather-inputs-fdd.sh
+++ machine_learning_hep/gather-inputs-fdd.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+MLHEP_DIR="/data8/majak/MLHEP"
+OUTPUT_DIR="${MLHEP_DIR}/pass6-input-fdd"
+
 for fd in `seq 0 10 90` ; do
-  cp /data2/mkabus/MLHEP/results-multiclass-fd_${fd}-bkg_rv/LHC22pp_mc/Results/prod_LHC22/resultsmctot/efficienciesLcpKpiRun3analysis.root \
-     /data2/mkabus/MLHEP/input-fdd/efficienciesLcpKpiRun3analysis-fd_${fd}.root
-  cp /data2/mkabus/MLHEP/results-multiclass-fd_${fd}-bkg_rv/LHC22pp/Results/resultsdatatot/Yields_LcpKpi_Run3analysis.root \
-     /data2/mkabus/MLHEP/input-fdd/yieldsLcpKpiRun3analysis-fd_${fd}.root
+  RESDIR="results-fd_${fd}-bkg_0524"
+
+  cp "${MLHEP_DIR}/${RESDIR}/LHC22pp_mc/Results/prod_LHC24d3/resultsmctot/efficienciesLcpKpiRun3analysis.root" \
+     "${OUTPUT_DIR}/efficienciesLcpKpiRun3analysis-fd_${fd}.root"
+  cp "${MLHEP_DIR}/${RESDIR}/LHC22pp/Results/resultsdatatot/Yields_LcpKpi_Run3analysis.root" \
+     "${OUTPUT_DIR}/yieldsLcpKpiRun3analysis-fd_${fd}.root"
 done
