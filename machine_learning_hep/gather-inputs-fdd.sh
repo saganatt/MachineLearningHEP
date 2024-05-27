a0c5e0b10cfa60def9359faac1e4ec11d146f771
saganatt <8majak8@gmail.com>
2024-05-27 18:15:18 +0200

    Rebin changed in pt 2-4

--- machine_learning_hep/gather-inputs-fdd.sh
+++ machine_learning_hep/gather-inputs-fdd.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 MLHEP_DIR="/data8/majak/MLHEP"
-OUTPUT_DIR="${MLHEP_DIR}/pass6-input-fdd"
+OUTPUT_DIR="${MLHEP_DIR}/pass6-input-fdd-rebin"
 
-for fd in `seq 0 10 90` ; do
-  RESDIR="results-fd_${fd}-bkg_0524"
+for fd in $(seq 0 5 90; seq 92 2 98) ; do
+  RESDIR="results-fd_${fd}-bkg_0524-rebin"
 
   cp "${MLHEP_DIR}/${RESDIR}/LHC22pp_mc/Results/prod_LHC24d3/resultsmctot/efficienciesLcpKpiRun3analysis.root" \
      "${OUTPUT_DIR}/efficienciesLcpKpiRun3analysis-fd_${fd}.root"
