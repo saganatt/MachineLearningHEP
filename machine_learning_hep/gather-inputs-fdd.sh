8f061fb384dc365aebbb824fc600cee08db002e0
saganatt <8majak8@gmail.com>
2024-06-12 17:31:21 +0200

    Changed paths in gather script

--- machine_learning_hep/gather-inputs-fdd.sh
+++ machine_learning_hep/gather-inputs-fdd.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 MLHEP_DIR="/data8/majak/MLHEP"
-OUTPUT_DIR="${MLHEP_DIR}/pass6-input-fd_precise_0524_13cuts_005bkg"
+OUTPUT_DIR="${MLHEP_DIR}/input-fd_precise_0624"
 
-RESDIR_PATTERN="${MLHEP_DIR}/results-fd_precise_0524_13cuts_005bkg_"
+RESDIR_PATTERN="${MLHEP_DIR}/results-fd_precise_0624_"
 
 for dir in ${RESDIR_PATTERN}* ; do
   suffix=${dir##${RESDIR_PATTERN}}
   echo $suffix
 
-  cp "${dir}/LHC22pp_mc/Results/prod_LHC24d3/resultsmctot/efficienciesLcpKpiRun3analysis.root" \
+  cp "${dir}/LHC22pp_mc/Results/prod_LHC24d3b/resultsmctot/efficienciesLcpKpiRun3analysis.root" \
      "${OUTPUT_DIR}/efficienciesLcpKpiRun3analysis-fd_${suffix}.root"
   cp "${dir}/LHC22pp/Results/resultsdatatot/Yields_LcpKpi_Run3analysis.root" \
      "${OUTPUT_DIR}/yieldsLcpKpiRun3analysis-fd_${suffix}.root"
