8ffa96bf9b4ac26b577283b1f1bf95476bf988fc
saganatt <8majak8@gmail.com>
2025-02-07 09:42:18 +0100

    Allow for more digits in fd cut histomass

--- machine_learning_hep/processerdhadrons.py
+++ machine_learning_hep/processerdhadrons.py
@@ -108,7 +108,7 @@ class ProcesserDhadrons(Processer): # pylint: disable=too-many-instance-attribut
             df_ptmerged = pd.concat([df_ptmerged, df], ignore_index=True)
 
             if self.mltype == "MultiClassification":
-                suffix = "%s%d_%d_%.2f%.2f%.2f" % \
+                suffix = "%s%d_%d_%.2f%.2f%.3f" % \
                          (self.v_var_binning, self.lpt_finbinmin[ipt],
                           self.lpt_finbinmax[ipt], self.lpt_probcutfin[ipt][0],
                           self.lpt_probcutfin[ipt][1], self.lpt_probcutfin[ipt][2])
