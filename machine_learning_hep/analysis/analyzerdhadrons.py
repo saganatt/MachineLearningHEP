b228fcaff51c6685075b92ef088bdf5458463167
saganatt <8majak8@gmail.com>
2025-03-14 18:23:18 +0100

    Print fit result earlier (and it's always null...)

--- machine_learning_hep/analysis/analyzerdhadrons.py
+++ machine_learning_hep/analysis/analyzerdhadrons.py
@@ -182,6 +182,7 @@ class AnalyzerDhadrons(Analyzer):  # pylint: disable=invalid-name
         if level == "data":
             mean_sgn = ws.var(self.p_param_names["gauss_mean"])
             sigma_sgn = ws.var(self.p_param_names["gauss_sigma"])
+            print(f"fit result:\n{res.Print()}")
             print(f"Significance for pt: {self.bins_candpt[ipt]} - {self.bins_candpt[ipt+1]}")
             (sig, sig_err, bkg, bkg_err,
             signif, signif_err, s_over_b, s_over_b_err
