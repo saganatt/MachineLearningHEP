cd1ab15843c844a92226e5d6834b1f325f5a4575
saganatt <8majak8@gmail.com>
2025-03-20 16:11:28 +0100

    Another debug log in roofitter

--- machine_learning_hep/fitting/roofitter.py
+++ machine_learning_hep/fitting/roofitter.py
@@ -61,6 +61,8 @@ class RooFitter:
     # pylint: disable=too-many-branches
     def fit_mass_new(self, hist, pdfnames, param_names, fit_spec, level, fixed_sigma, fixed_sigma_val,
                      roows = None, plot = False):
+        print(f"fit spec:\n{fit_spec}")
+        print(f'fitting for level {level} pt: {fit_spec.get("ptrange", "")}')
         if hist.GetEntries() == 0:
             raise UserWarning('Cannot fit histogram with no entries')
         ws = roows or ROOT.RooWorkspace("ws")
@@ -83,14 +85,12 @@ class RooFitter:
         print(f"m var: {m}")
 
         if level == "mc":
-            print(f"fit spec\n{fit_spec}")
             sigma_sgn = ws.var(param_names["gauss_sigma"])
             if fixed_sigma:
                 sigma_sgn.setVal(fixed_sigma_val)
                 sigma_sgn.setConstant(True)
 
         if level == "data":
-            print(f"fit spec\n{fit_spec}")
             signal_pdf = ws.pdf(pdfnames["pdf_sig"])
             if not signal_pdf:
                 raise ValueError("sig PDF not found")
